890b97695e5698dc2bd3bbd037bc1d4e9939b7c4
Felipe-gsilva <felipe.gsilva@protonmail.com>
2025-12-30 11:10:41 -0300

    things for the notebook

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,3 +7,4 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 # Launch polybar with a specific configuration file
 POLYBAR_CONFIG="$HOME/.config/polybar/config.ini"
 polybar --config="$POLYBAR_CONFIG" main &
+polybar --config="$POLYBAR_CONFIG" secondary &
